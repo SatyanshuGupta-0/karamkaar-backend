ce97a4d341b02df6a1ea19c74512e53f57f6e4f6
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2026-07-22 22:27:03 +0530

    modile field remove in googleauth

--- src/DB/db.js
+++ src/DB/db.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-  console.log("MongoDB Connected");
-})
-.catch((err) => {
-  console.log(err);
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(async () => {
+    console.log("MongoDB Connected");
+
+    console.log("Database:", mongoose.connection.name);
+
+    const indexes = await mongoose.connection.db
+      .collection("users")
+      .indexes();
+
+    console.log(indexes);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
